59b857d09b5753ad707180955ebffa75557bdbae
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2025-04-18 21:31:47 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = ({toggleReg}) => {
                 <div className='text-center'>
                     <h1 className='text-[white] text-[64px] font-bold'>E-Summit'25</h1>
                     <h5 className='font-semibold text-[grey] text-[32px]'>1st & 2nd May 2025</h5>
-                    <button onClick={toggleReg} className='border-[3px] text-[#101656] border-[#101656] bg-white p-2 pl-5 pr-5 text-2xl m-3 hover:bg-[#101656] hover:text-[white] rounded-lg font-semibold'>Register</button>
+                    <button onClick={toggleReg} className='border-[3px] text-[#101656] border-white hover:border-[#101656] bg-white p-2 pl-5 pr-5 text-2xl m-3 hover:bg-[#101656] hover:text-[white] rounded-lg font-semibold'>Register</button>
                 </div>
             </div>
             <div className='flex justify-between items-center w-full mb-36 mt-36 md:mb-32 md:mt-7'>
